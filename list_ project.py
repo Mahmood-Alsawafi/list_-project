9293395357a62c2b5f8b0daffdf6230987868470
Mahmood <msssals54321@gmail.com>
2025-05-15 14:53:36 +0400

    oman this

---
+++ list_ project.py
@@ -0,0 +1,11 @@
+list_number =[10, 20, 30, 40, 50]
+added_number= 0
+list_number.append( added_number)
+while input!=("done"):
+    added_number= int(input("please enter the number: "))
+    list_number.append( added_number)
+    print(list_number)
+    
+
+
+print(list_number)
